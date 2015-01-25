82446d8050758c7e876e82d5d07b9984c66ca206
potatocubes <zonedabone+potatocubes@gmail.com>
2015-01-24 19:08:44 -0500

    Update spookyghost.js

--- spookyghost.js
+++ spookyghost.js
@@ -5,13 +5,14 @@
 // @description  enter something useful
 // @author       You
 // @match        http://this.spooks.me/
+// @require      https://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.3.11/seedrandom.min.js
 // @grant        none
 // ==/UserScript==
 
 latest = null
 
 CLIENT.on("message", function(message){
-    var flaireg = /(#(?:(?:alice|cadet|cornflower|dark(?:slate)?|deepsky|dodger|light(?:sky|steel)?|medium(?:slate)?|midnight|powder|royal|sky|slate|steel)?blue|(?:antique|floral|ghost|navajo)?white|aqua|(?:medium)?aquamarine|azure|beige|bisque|black|blanchedalmond|(?:blue|dark)?violet|(?:rosy|saddle|sandy)?brown|burlywood|chartreuse|chocolate|(?:light)?coral|cornsilk|crimson|(?:dark|light)?cyan|(?:dark|pale)?goldenrod|(?:dark(?:slate)?|dim|light(?:slate)?|slate)?gr(?:a|e)y|(?:dark(?:olive|sea)?|forest|lawn|light(?:sea)?|lime|medium(?:sea|spring)|pale|sea|spring|yellow)?green|(?:dark)?khaki|(?:dark)?magenta|(?:dark)?orange|(?:medium|dark)?orchid|(?:dark|indian|(?:medium|pale)?violet|orange)?red|(?:dark|light)?salmon|(?:dark|medium|pale)?turquoise|(?:deep|hot|light)?pink|firebrick|fuchsia|gainsboro|gold|(?:green|light(?:goldenrod)?)?yellow|honeydew|indigo|ivory|lavender(?:blush)?|lemonchiffon|lime|linen|maroon|(?:medium)?purple|mintcream|mistyrose|moccasin|navy|oldlace|olive(?:drab)?|papayawhip|peachpuff|peru|plum|seashell|sienna|silver|snow|tan|teal|thistle|tomato|wheat|whitesmoke)|#(?:[a-f0-9]{3}){1,2}|\/\!\!|\/[+^\-~*#@%_!]|\$.+?\||\|)|(.)/gi;
+    var flaireg = /(#?#(?:(?:alice|cadet|cornflower|dark(?:slate)?|deepsky|dodger|light(?:sky|steel)?|medium(?:slate)?|midnight|powder|royal|sky|slate|steel)?blue|(?:antique|floral|ghost|navajo)?white|aqua|(?:medium)?aquamarine|azure|beige|bisque|black|blanchedalmond|(?:blue|dark)?violet|(?:rosy|saddle|sandy)?brown|burlywood|chartreuse|chocolate|(?:light)?coral|cornsilk|crimson|(?:dark|light)?cyan|(?:dark|pale)?goldenrod|(?:dark(?:slate)?|dim|light(?:slate)?|slate)?gr(?:a|e)y|(?:dark(?:olive|sea)?|forest|lawn|light(?:sea)?|lime|medium(?:sea|spring)|pale|sea|spring|yellow)?green|(?:dark)?khaki|(?:dark)?magenta|(?:dark)?orange|(?:medium|dark)?orchid|(?:dark|indian|(?:medium|pale)?violet|orange)?red|(?:dark|light)?salmon|(?:dark|medium|pale)?turquoise|(?:deep|hot|light)?pink|firebrick|fuchsia|gainsboro|gold|(?:green|light(?:goldenrod)?)?yellow|honeydew|indigo|ivory|lavender(?:blush)?|lemonchiffon|lime|linen|maroon|(?:medium)?purple|mintcream|mistyrose|moccasin|navy|oldlace|olive(?:drab)?|papayawhip|peachpuff|peru|plum|seashell|sienna|silver|snow|tan|teal|thistle|tomato|wheat|whitesmoke)|#?#(?:[a-f0-9]{3}){1,2}|\/\!\!|\/[+^\-~*#@%_!]|\$.+?\||\|)|(.)/gi;
     var coloreg = /(?:^|[^#])#((?:alice|cadet|cornflower|dark(?:slate)?|deepsky|dodger|light(?:sky|steel)?|medium(?:slate)?|midnight|powder|royal|sky|slate|steel)?blue|(?:antique|floral|ghost|navajo)?white|aqua|(?:medium)?aquamarine|azure|beige|bisque|black|blanchedalmond|(?:blue|dark)?violet|(?:rosy|saddle|sandy)?brown|burlywood|chartreuse|chocolate|(?:light)?coral|cornsilk|crimson|(?:dark|light)?cyan|(?:dark|pale)?goldenrod|(?:dark(?:slate)?|dim|light(?:slate)?|slate)?gr(?:a|e)y|(?:dark(?:olive|sea)?|forest|lawn|light(?:sea)?|lime|medium(?:sea|spring)|pale|sea|spring|yellow)?green|(?:dark)?khaki|(?:dark)?magenta|(?:dark)?orange|(?:medium|dark)?orchid|(?:dark|indian|(?:medium|pale)?violet|orange)?red|(?:dark|light)?salmon|(?:dark|medium|pale)?turquoise|(?:deep|hot|light)?pink|firebrick|fuchsia|gainsboro|gold|(?:green|light(?:goldenrod)?)?yellow|honeydew|indigo|ivory|lavender(?:blush)?|lemonchiffon|lime|linen|maroon|(?:medium)?purple|mintcream|mistyrose|moccasin|navy|oldlace|olive(?:drab)?|papayawhip|peachpuff|peru|plum|seashell|sienna|silver|snow|tan|teal|thistle|tomato|wheat|whitesmoke|(?:[a-f0-9]{3}){1,2})/i
     var bgreg = /##((?:alice|cadet|cornflower|dark(?:slate)?|deepsky|dodger|light(?:sky|steel)?|medium(?:slate)?|midnight|powder|royal|sky|slate|steel)?blue|(?:antique|floral|ghost|navajo)?white|aqua|(?:medium)?aquamarine|azure|beige|bisque|black|blanchedalmond|(?:blue|dark)?violet|(?:rosy|saddle|sandy)?brown|burlywood|chartreuse|chocolate|(?:light)?coral|cornsilk|crimson|(?:dark|light)?cyan|(?:dark|pale)?goldenrod|(?:dark(?:slate)?|dim|light(?:slate)?|slate)?gr(?:a|e)y|(?:dark(?:olive|sea)?|forest|lawn|light(?:sea)?|lime|medium(?:sea|spring)|pale|sea|spring|yellow)?green|(?:dark)?khaki|(?:dark)?magenta|(?:dark)?orange|(?:medium|dark)?orchid|(?:dark|indian|(?:medium|pale)?violet|orange)?red|(?:dark|light)?salmon|(?:dark|medium|pale)?turquoise|(?:deep|hot|light)?pink|firebrick|fuchsia|gainsboro|gold|(?:green|light(?:goldenrod)?)?yellow|honeydew|indigo|ivory|lavender(?:blush)?|lemonchiffon|lime|linen|maroon|(?:medium)?purple|mintcream|mistyrose|moccasin|navy|oldlace|olive(?:drab)?|papayawhip|peachpuff|peru|plum|seashell|sienna|silver|snow|tan|teal|thistle|tomato|wheat|whitesmoke|(?:[a-f0-9]{3}){1,2})/i
     var fontreg = /\$([\w \-\,Â®]*)\|/
@@ -20,14 +21,17 @@ CLIENT.on("message", function(message){
     if(message.nick != CLIENT.attributes.nick && message.flair){
         console.log(message)
         var parts = []
+        nick = ""
         var isChar = true
         var flair = {bg:"",
                      color: "",
-                     font: ""}
+                     font: "",
+                     original: message.flair}
         while((found = flaireg.exec(message.flair)) !== null){
             console.log(found)
             if(found[2]){
                 isChar = true
+                nick += found[2]
             }else{
                 if(isChar){
                     isChar = false
@@ -37,6 +41,10 @@ CLIENT.on("message", function(message){
                 }
             }
         }
+        console.log(parts)
+        /*if(nick != message.nick){
+            parts = []
+        }*/
         flair.parts = parts
         if((found = coloreg.exec(message.message)) !== null){
             flair.color = found[1]
@@ -47,7 +55,7 @@ CLIENT.on("message", function(message){
         if((found = fontreg.exec(message.message)) !== null){
             flair.font = found[1]
         }
-        if(flair.parts.length){
+        if(flair.parts.length || flair.color || flair.bg || flair.font){
             latest = flair
         }
     }
@@ -65,19 +73,25 @@ CLIENT.submit = function(message){
         return;
     }
     var parts = latest.parts.slice()
-    while(chars.length > parts.length){
-        var index = Math.floor(Math.random() * (chars.length-1));
-        chars.splice(index, 2, chars[index]+chars[index+1]);
+    if(parts.length != 0){
+        Math.seedrandom(latest.original)
+        while(chars.length > parts.length){
+            var index = Math.floor(Math.random() * (chars.length-1));
+            chars.splice(index, 2, chars[index]+chars[index+1]);
+        }
+        while(parts.length > chars.length){
+            var index = Math.floor(Math.random() * (parts.length-1));
+            parts.splice(index, 2, parts[index]+parts[index+1]);
+        }
+        final = ""
+        for(var i = 0;i<chars.length;i++){
+            final += parts[i]
+            final += chars[i]
+        }
+    }else{
+        final = CLIENT.attributes.nick
     }
-    while(parts.length > chars.length){
-        var index = Math.floor(Math.random() * (parts.length-1));
-        parts.splice(index, 2, parts[index]+parts[index+1]);
-    }
-    final = ""
-    for(var i = 0;i<chars.length;i++){
-        final += parts[i]
-        final += chars[i]
-    }
+    
     CLIENT.attributes.font = latest.font
     if(latest.bg && latest.color){
         CLIENT.attributes.color = "#"+latest.bg+"#"+latest.color
